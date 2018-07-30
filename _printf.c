e7587190e885373bd4d0c9410d0952a5767f7adf
Jenn <jennogden95@gmail.com>
2018-07-30 22:00:57 +0000

    Returned it back to normal

--- _printf.c
+++ _printf.c
@@ -14,30 +14,31 @@ int _printf(const char *format, ...)
 	int i, j = 0, len, count = 0;
 	va_list val;
 
-	/*
-	 *if (format == NULL)
-	 *	return (-1);
-	 */
+	
+	if (format == NULL)
+		return (-1);
+	
 	va_start(val, format);
-	while (format && format[j])
-	{
-		if (format[j] == '%')
-		{
-			switch (format[j + 1])
-			{
-				case '%':
-				case 'c':
-				case 's':
-				case 'i':
-				case 'd':
-					i++;
-					break;
-				default:
-					return (-1);
-			}
-		}
-		j++;
-	}
+	/*
+	 *while (format && format[j])
+	 *{
+	 *	if (format[j] == '%')
+	 *	{
+	 *		switch (format[j + 1])
+	 *		{
+	 *			case '%':
+	 *			case 'c':
+	 *			case 's':
+	 *			case 'i':
+	 *			case 'd':
+	 *				i++;
+	 *				break;
+	 *			default:
+	 *				return (-1);
+	 *		}
+	 *	}
+	 *	j++;
+	 }*/
 	for (i = 0; format[i]; i++)
 	{
 		if (format[i] == '%')
